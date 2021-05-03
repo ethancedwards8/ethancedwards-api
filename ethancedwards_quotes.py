76e61be60b9253ba806d0177110345af81173c5b
Ethan Edwards <ethan@ethancedwards.com>
2021-05-03 19:34:36 -0400

    Change list to dictionairy, fix get def to specify quote

--- ethancedwards_quotes.py
+++ ethancedwards_quotes.py
@@ -7,93 +7,79 @@ import random
 app = Flask(__name__)
 api = Api(app)
 
-quotes = [
-    {
-        "id": 0,
+quotes = {
+    0: {
         "author": "Linus Torvalds",
         "quote": "Once you start thinking more about where you want to be than about making the best product, you're screwed."
     },
-    {
-        "id": 1,
+    1: {
         "author": "Edward Snowden",
         "quote": "Arguing that you don\'t care about privacy because you have nothing to hide is no different than saying you don\'t care about free speech because you have nothing to say."
     },
-    {
-        "id": 2,
+    2: {
         "author": "Terry A. Davis",
         "quote": "An idiot admires complexity, a genius admires simplicity."
     },
-    {
-        "id": 3,
+    3: {
         "author": "Peter Drucker",
         "quote": "The best way to predict the future is to create it."
     },
-    {
-        "id": 4,
-        "author": "Edward Snowden",
-        "quote": "Privacy matters; privacy is what allows us to determine who we are and who we want to be."
-    },
-    {
-        "id": 5,
+    4: {
         "author": "Linus Torvalds",
         "quote": "Those that can, do. Those that can\'t, complain.",
     },
-    {
-        "id": 6,
+    5: {
+        "author": "Linus Torvalds",
+        "quote": "Those that can, do. Those that can\'t, complain.",
+    },
+    6: {
         "author": "Richard Stallman",
         "quote": "Creativity can be a social contribution, but only in so so far as society is free to use the results.",
     },
-    {
-        "id": 7,
+    7: {
         "author": "Steve Prefontaine",
         "quote": "To give anything less than your best is to sacrifice the gift.",
     },
-    {
-        "id": 8,
+    8: {
         "author": "Grant Cardone",
         "quote": "Fear is an indicator of moving in the right direction."
     },
-    {
-        "id": 9,
+    9: {
         "author": "Casey Neistat",
         "quote": "The Grind is not Glamorous.",
     },
-    {
-        "id": 10,
+    10: {
         "author": "Alan Lakein",
         "quote": "Failing to plan is planning to fail."
     },
-    {
-        "id": 11,
+    11: {
         "author": "Chip Heath",
         "quote": "The Curse of Knowledge: when we are given knowledge, it is impossible to imagine what it's like to LACK that knowledge."
     },
-    {
-        "id": 12,
+    12: {
         "author": "Russian Proverb",
         "quote": "The shark that does not swims, drowns."
     },
-    {
-        "id": 13,
+    13: {
         "author": "Yoda",
         "quote": "Do, or do not. There is no try."
     },
-    {
-        "id": 14,
+    14: {
         "author": "Henry Ford",
         "quote": "Whether you think you can or think you can't you're right"
     },
-]
+}
 
 class Quote(Resource):
     def get(self, id=0):
         if id == 0:
             return random.choice(quotes), 200
+        quote = quotes.get(id)
 
-        for quote in quotes:
-            if(quote["id"] == id):
-                return qoute, 200
-        return "Quote not found", 404
+        if quote:
+            return quote, 200
+
+        return "quote not found", 404
 
 api.add_resource(Quote, "/quotes", "/quotes/", "/quotes/<int:id>")
 
