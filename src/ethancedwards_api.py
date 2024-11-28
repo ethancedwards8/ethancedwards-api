65cbbdcd98a026873df670376192a999d6f80ad7
Ethan Edwards <ethan@ethancedwards.com>
2024-11-28 18:50:54 -0500

    deal with CORS

--- src/ethancedwards_api.py
+++ src/ethancedwards_api.py
@@ -17,6 +17,14 @@ app = Flask(__name__)
 api = Api(app)
 DEV = os.environ.get('DEV')
 
+# deal with CORS crap
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
 # quote resources being added
 api.add_resource(Quote, "/quotes/v1", "/quotes/v1/", "/quotes/v1/<int:id>", "/quotes/v1/<int:id>/")
 api.add_resource(Dump, "/quotes/v1/dump", "/quotes/v1/dump/")
