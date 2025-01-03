5adad24b1a8aa237649c9162c4bb532c8b06277e
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-01-03 11:20:20 -0500

    remove Nix stuff

--- default.nix
+++
@@ -1,9 +0,0 @@
-with import <nixpkgs> {};
-
-python3Packages.buildPythonApplication rec {
-  pname = "ethancedwards-quotes";
-  version = "1.0";
-  src = ./.;
-
-  propagatedBuildInputs = with python3Packages; [ flask flask-restful ];
-}
