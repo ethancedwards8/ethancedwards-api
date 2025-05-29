6211ddcb8a26375263db82966172da0493350a99
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-05-28 22:17:44 -0400

    add approval argument

--- ethancedwards_api/nixpkgs/nixpkgs.py
+++ ethancedwards_api/nixpkgs/nixpkgs.py
@@ -17,14 +17,16 @@ github.headers.update({ "Authorization": f"Bearer {GITHUB_KEY}" })
 
 parser = reqparse.RequestParser()
 parser.add_argument('pr_num', required=True, type=str, location='args')
+parser.add_argument('approve', required=False, type=str, location='args', default=False)
 
 class WorkflowRun(Resource):
     def post(self):
         args = parser.parse_args()
 
         pr_num = args['pr_num']
+        approve = args['approve']
 
         req = github.post(GITHUB_API + '/repos/ethancedwards8/nixpkgs-review-gha/actions/workflows/review.yml/dispatches',
-                    json={'ref': 'main', 'inputs': {'pr': str(pr_num) }})
+                          json={'ref': 'main', 'inputs': {'pr': str(pr_num), 'approve-on-success': approve }})
 
         return f"success for {pr_num}", 200
