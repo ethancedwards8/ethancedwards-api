67009adf798d0a9ec92cb5ec2bd974a830761141
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-12-19 17:16:33 -0500

    api changed on me. fix. :(
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- ethancedwards_api/nixpkgs/nixpkgs.py
+++ ethancedwards_api/nixpkgs/nixpkgs.py
@@ -17,16 +17,16 @@ github.headers.update({ "Authorization": f"Bearer {GITHUB_KEY}" })
 
 parser = reqparse.RequestParser()
 parser.add_argument('pr_num', required=True, type=str, location='args')
-parser.add_argument('approve', required=False, type=str, location='args', default=False)
+parser.add_argument('on_success', required=False, type=str, location='args', default="nothing")
 
 class WorkflowRun(Resource):
     def post(self):
         args = parser.parse_args()
 
         pr_num = args['pr_num']
-        approve = args['approve']
+        on_success = args['on_success']
 
         req = github.post(GITHUB_API + '/repos/ethancedwards8/nixpkgs-review-gha/actions/workflows/review.yml/dispatches',
-                          json={'ref': 'main', 'inputs': {'pr': str(pr_num), 'approve-on-success': approve }})
+                          json={'ref': 'main', 'inputs': {'pr': str(pr_num), 'on-success': on_success }})
 
         return f"success for {pr_num}", 200
