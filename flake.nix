a4de285102872b08b9bd1507b06e589bf668a80e
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-12-24 21:22:17 -0500

    flake.nix setup for dev shell
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- flake.nix
+++ flake.nix
@@ -1,65 +1,39 @@
 {
-  description = "API for Ethan";
+  description = "Ethan's Api Dev Flake";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/master";
-  inputs.nur.url = "github:nix-community/nur";
-  inputs.nur.inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
-  inputs.pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
-  inputs.pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
-
-  outputs =
-    { nixpkgs, pyproject-nix, ... }:
-    let
-      # Loads pyproject.toml into a high-level project representation
-      # Do you notice how this is not tied to any `system` attribute or package sets?
-      # That is because `project` refers to a pure data representation.
-      project = pyproject-nix.lib.project.loadPyproject {
-        # Read & unmarshal pyproject.toml relative to this project root.
-        # projectRoot is also used to set `src` for renderers such as buildPythonPackage.
-        projectRoot = ./.;
-      };
-
-      # This example is only using aarch64-darwin
-      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-
-      # We are using the default nixpkgs Python3 interpreter & package set.
-      #
-      # This means that you are purposefully ignoring:
-      # - Version bounds
-      # - Dependency sources (meaning local path dependencies won't resolve to the local path)
-      #
-      # To use packages from local sources see "Overriding Python packages" in the nixpkgs manual:
-      # https://nixos.org/manual/nixpkgs/stable/#reference
-      #
-      # Or use an overlay generator such as uv2nix:
-      # https://github.com/pyproject-nix/uv2nix
-      python = pkgs.python3;
-
-    in
+  outputs = inputs@{ self, nixpkgs, ... }:
+  let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+  in
     {
-      # Create a development shell containing dependencies from `pyproject.toml`
-      devShells.aarch64-darwin.default =
+      devShell = forAllSystems (system:
         let
-          # Returns a function that can be passed to `python.withPackages`
-          arg = project.renderers.withPackages { inherit python; };
-
-          # Returns a wrapped environment (virtualenv like) with all our packages
-          pythonEnv = python.withPackages arg;
-
+          pkgs = import nixpkgs { inherit system; };
         in
-        # Create a devShell like normal.
-        pkgs.mkShell { packages = [ pythonEnv ]; };
+        with pkgs;
+        mkShell {
+          name = "dev shell";
 
-      # Build our package using `buildPythonPackage
-      packages.aarch64-darwin.default =
-        let
-          # Returns an attribute set that can be passed to `buildPythonPackage`.
-          attrs = project.renderers.buildPythonPackage { inherit python; };
-        in
-        # Pass attributes to buildPythonPackage.
-        # Here is a good spot to add on any missing or custom attributes.
-        python.pkgs.buildPythonPackage (attrs // { env.CUSTOM_ENVVAR = "hello"; });
+          nativeBuildInputs = [ uv ];
+
+          build-system = with python3Packages; [
+              hatchling
+          ];
+
+          dependencies = with python312Packages; [
+              flask
+              flask-restful
+              yt-dlp
+              podgen
+              feedparser
+              pygeocodio
+              jobspy
+          ];
+        }
+      );
     };
 }
-
